f3739d6dff59f58c071290f16b9d121187d365b3
Benjamin Wilson <Seprothia@Benjamins-MacBook-Pro.local>
2018-01-03 20:24:04 +0000

    Product collection view added

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Benjamin Wilson on 03/01/2018.
+//  Copyright © 2018 Benjamin Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
